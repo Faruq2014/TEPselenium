a9947d0775bff99b1c1ee96448f31d9e70546d79
Faruq2014 <mjmolla@gmail.com>
2019-03-21 17:21:55 -0400

    First commit.

---
+++ TEPselenium/src/browserCommand/GetCommand.java
@@ -0,0 +1,16 @@
+package browserCommand;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GetCommand {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\SeleniumDriver\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		
+
+	}
+
+}
